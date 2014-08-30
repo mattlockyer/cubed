1df4773b1ee569d50e6baee88a4de63a9a25a047
Matt Lockyer <mattdlockyer@gmail.com>
2014-08-29 22:14:25 -0700

    Something is off when you rotate and then rotate the other direction... not sure what.
    inconsistency between move and transformations perhaps... otherwise, really close to solution.
    
    Try rotating 1, 2, or 3 times in one direction, other rotations are off, when you are back to canonical, everything is fine...

--- main.js
+++ main.js
@@ -17,14 +17,15 @@ bounds = {
 	near:-halfObjSize
 };
 
-var body = document.body;
+var container = document.getElementById('c');
 var contexts = [];
 
 var graphics = {};
 
-var object;
+var objects;
 
 var speed = 0.8;
+
 var moves = {
 	37:new Vector(-speed, 0),
 	38:new Vector(0, -speed),
@@ -32,23 +33,105 @@ var moves = {
 	40:new Vector(0, speed)
 };
 
+var transformVals = [
+[0, 0, 0, 0, 0, 0],
+[0, -160, -160, 90, 0, 0],
+[0, 0, -320, 0, 180, 0],
+[0, 160, -160, -90, 0, 0],
+[-160, 0, -160, 0, -90, 0],
+[160, 0, -160, 0, 90, 0]
+];
+
+var csv = [0, 1, 2, 3];
+var csh = [0, 4, 2, 5];
+
+/*******************************
+* Something is off when you rotate and then rotate the other direction... not sure what.
+
+inconsistency between move and transformations perhaps... otherwise, really close to solution.
+
+Try rotating 1, 2, or 3 times in one direction, other rotations are off, when you are back to canonical, everything is fine...
+*******************************/
+
+var move = function(cur, vert, amount) {
+	var arr = (vert) ? csv : csh;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] === cur) break;
+	}
+	i += amount;
+	if (i < 0) i = 3;
+	if (i > 3) i = 0;
+	return arr[i];
+};
+
+var reconcileRotationV = function() {
+	csh[0] = csv[0];
+	csh[2] = csv[2];
+};
+var reconcileRotationH = function() {
+	csv[0] = csh[0];
+	csv[2] = csh[2];
+};
+
+var rotate = function(dir) {
+	switch (dir) {
+		case 0:
+		csv.push(csv.shift());
+		reconcileRotationV();
+		break;
+		case 1:
+		csv.unshift(csv.pop());
+		reconcileRotationV();
+		break;
+		case 2:
+		csh.push(csh.shift());
+		reconcileRotationH();
+		break;
+		case 3:
+		csh.unshift(csh.pop());
+		reconcileRotationH();
+		break;
+	}
+	console.log(csv);
+	console.log(csh);
+};
+
 /*******************************
 * Main Game Loop
 *******************************/
 
 var animate = function() {
+	for (var i = 0; i < 6; i++) {
+		var g = contexts[i];
+		g.clearRect(0, 0, size, size);
+
+		g.fillStyle = colors[i];
+		//g.fillRect(size/2 - 40, size/2 - 40, 80, 80);
+
+		var ti = (i < 4) ? csv[i] : (i === 4) ? csh[1] : csh[3];
+
+		transform(g.canvas, transformVals[ti]);
+
+		object.u(g, i);
+		object.um(g, i);
+
+		if (object.c !== i) continue;
+
+		for (var k = 37; k < 41; k++)
+			if (keys[k]) object.a.a(moves[k]);
+
+		if (keys[32]) object.f();
+		if (keys[65]) object.m = [];
+
+	}
+
 	requestAnimationFrame(animate);
+};
 
-	var g = contexts[0];
-	g.clearRect(0, 0, size, size);
-
-	for (var i = 37; i < 41; i++)
-		if (keys[i]) object.a.a(moves[i]);
-
-	if (keys[32]) object.f();
-	if (keys[65]) object.m = [];
-	
-	object.u(g);
+var transform = function(el, args) {
+	var s = el.style;
+	s.transform = 'perspective(320px) translate3d(' + args[0] + 'px, ' + args[1] + 'px, '
+		+ args[2] + 'px) rotateX(' + args[3] + 'deg) rotateY(' + args[4] + 'deg) rotateZ(' + args[5] + 'deg)';
 };
 
 /*******************************
@@ -60,13 +143,14 @@ window.onload = function() {
 	for (var i = 0; i < 6; i++) {
 		var c = document.createElement('canvas');
 		c.width = c.height = size;
-		body.appendChild(c);
+		container.appendChild(c);
 		contexts.push(c.getContext('2d'));
 	}
 
 	genGraphics(contexts[0]);
 
 	object = new Obj(graphics.y.tri);
+	object.z = true;
 	object.p.s(halfSize, size);
 
 	animate();
--- obj.js
+++ obj.js
@@ -1,19 +1,22 @@
 
 
-var Obj = function(img, p, v, a, d) {
+var Obj = function(img, p, v, a, d, c) {
 	var t = this;
 	t.p = p || new Vector(0, 0);
 	t.v = v || new Vector(0, 0);
 	t.a = a || new Vector(0, 0);
 	t.r = 0;
 	t.d = d || 0.9;
+	this.c = c || 0;
 	t.i = img;
 	t.m = [];
+	t.z = false;
+	t.ft = 0;
 };
 
 Obj.prototype = {
 	constructor:Obj,
-	u:function(g) {
+	u:function(g, c) {
 		var t = this,
 		p = t.p,
 		v = t.v,
@@ -23,26 +26,47 @@ Obj.prototype = {
 		p.a(v);
 		v.m(t.d);
 		a.s(0, 0);
+		//firetimer
+		if (t.ft > 0) t.ft--;
 		//bounds
-		if (p.x > bounds.far) p.x = bounds.near;
-		if (p.x < bounds.near) p.x = bounds.far;
-		if (p.y > bounds.far) p.y = bounds.near;
-		if (p.y < bounds.near) p.y = bounds.far;
+		if (p.y < bounds.near) {
+			p.y = bounds.far;
+			t.c = move(t.c, true, -1);
+			if (t.z) rotate(0);
+		} else if (p.y > bounds.far) {
+			p.y = bounds.near;
+			t.c = move(t.c, true, 1);
+			if (t.z) rotate(1);
+		} else if (p.x < bounds.near) {
+			p.x = bounds.far;
+			t.c = move(t.c, false, -1);
+			if (t.z) rotate(2);
+		} else if (p.x > bounds.far) {
+			p.x = bounds.near;
+			t.c = move(t.c, false, 1);
+			if (t.z) rotate(3);
+		}
 		//rotation
 		t.r = Math.atan2(v.y, v.x) + HALF_PI;
+		//if this is not your canvas, don't draw
+		if (t.c !== c) return;
 		//draw
 		g.save();
 		g.translate(p.x, p.y);
 		g.rotate(t.r);
 		g.drawImage(t.i, -halfObjSize, -halfObjSize);
 		g.restore();
-
+	},
+	um:function(g, c) {
+		var t = this;
 		for (var i = 0; i < t.m.length; i++)
-			t.m[i].u(g);
+			t.m[i].u(g, c);
 	},
 	f:function() {
 		var t = this;
-		t.m.push(new Obj(graphics.b.tri, new Vector(t.p.x, t.p.y), new Vector(t.v.x, t.v.y), null, 1));
-		console.log(t.m.length);
+		if (t.ft === 0) {
+			t.ft = 60;
+			t.m.push(new Obj(graphics.b.tri, new Vector(t.p.x, t.p.y), new Vector(t.v.x, t.v.y), null, 1, t.c));
+		}
 	}
 }
